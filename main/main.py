bf9dce9a2a2dbaf4f4eecba43e55aa019764ebc9
nsde <neostyxde@gmail.com>
2020-09-25 16:07:09 +0200

    .

--- main/main.py
+++ main/main.py
@@ -1,17 +1,23 @@
 import os
 import tkinter as tk
 
+# Theme
 fgColor = "white"
 bgColor = "#212121"
 menuColor = "#091db7"
 activeColor = "#3f3f3f"
 reliefStyle = "flat"
 
+# Window
 win = tk.Tk()
 win.title("Styx Minecraft Launcher")
 win.config(bg=bgColor)
 
-print(os.getenv("JAVA.HOME"))
+# Check Java Path
+os.system('where java > javapath.txt')
+with open (os.getcwd() + '\\javapath.txt') as outlog:
+    outdata = outlog.readlines()
+javapath = outdata[0]
 
 def start():
     pass
