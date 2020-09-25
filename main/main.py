badee9b4b315f8f30ebfbe874ab04bea13d0f016
nsde <neostyxde@gmail.com>
2020-09-25 16:01:20 +0200

    commit

--- main/main.py
+++ main/main.py
@@ -1,1 +1,25 @@
+import os
+import tkinter as tk
 
+fgColor = "white"
+bgColor = "#212121"
+menuColor = "#091db7"
+activeColor = "#3f3f3f"
+reliefStyle = "flat"
+
+win = tk.Tk()
+win.title("Styx Minecraft Launcher")
+win.config(bg=bgColor)
+
+print(os.getenv("JAVA.HOME"))
+
+def start():
+    pass
+
+titleTxt = tk.Label(win, text="Styx MCL", font=('Calibri Light', 50), bg=bgColor, fg=fgColor)
+titleTxt.pack()
+
+startBtn = tk.Button(win, text="Start", command=start, font=('Calibri Light', 20), bg=bgColor, fg=fgColor, relief=reliefStyle, activebackground=activeColor)
+startBtn.pack()
+
+win.mainloop()
